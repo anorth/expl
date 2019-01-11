8615a5cb423a0b59038f5c413aa99dddcc16c609
Alex North <alex@alexn.id.au>
2019-01-11 14:50:30 +0000

    Added a simple fibonacci benchmark. Currently fib(30) in ~220ms

--- core/src/main/kotlin/org/explang/truffle/compiler/Testing.kt
+++ core/src/main/kotlin/org/explang/truffle/compiler/Testing.kt
@@ -3,22 +3,16 @@ package org.explang.truffle.compiler
 import com.oracle.truffle.api.Truffle
 import com.oracle.truffle.api.frame.FrameDescriptor
 import org.explang.syntax.TestParser
+import org.explang.truffle.nodes.ExpressionNode
 
 class TestCompiler(private val debug: Boolean) {
   private val parser = TestParser(debug)
   private val compiler = Compiler(debug)
 
-  fun eval(expression: String): Any {
+  fun compile(expression: String): ExpressionNode {
     val parse = parser.parse(expression)
     try {
-      val truffleEntry = compiler.compile(parse.syntax!!)
-      val topFrame = Truffle.getRuntime().createVirtualFrame(arrayOfNulls(0), FrameDescriptor())
-      val result = truffleEntry.executeDeclaredType(topFrame)
-      if (debug) {
-        println("*Result*")
-        println(result)
-      }
-      return result
+      return compiler.compile(parse.syntax!!)
     } catch (e: CompileError) {
       println("*Compile failed*")
       println(expression)
@@ -26,5 +20,19 @@ class TestCompiler(private val debug: Boolean) {
       println(e.message)
       throw e
     }
+  }
+
+  fun eval(expression: String): Any {
+    return eval(compile(expression))
+  }
+
+  fun eval(node: ExpressionNode): Any {
+    val topFrame = Truffle.getRuntime().createVirtualFrame(arrayOfNulls(0), FrameDescriptor())
+    val result = node.executeDeclaredType(topFrame)
+    if (debug) {
+      println("*Result*")
+      println(result)
+    }
+    return result
   }
 }
